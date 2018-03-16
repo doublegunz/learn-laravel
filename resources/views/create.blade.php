acb2db1a94871ec3c302812ab8b11b59cc422082
doublegunz <gungunpriatna02@gmail.com>
2018-03-16 20:36:15 +0700

    add login and register module

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -1,61 +1,163 @@
-<!-- create.blade.php -->
-
 <!DOCTYPE html>
-<html>
+<html lang="{{ app()->getLocale() }}">
 <head>
-<meta charset="utf-8">
-<title>Laravel 5.6 CRUD Tutorial With Example  </title>
-<link rel="stylesheet" href="{{asset('css/app.css')}}">
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">  
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
- 
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-  <div class="container">
-  <h2>Data Siswa</h2><br/>
-  <form method="post" action="{{url('siswa')}}" >
-  @csrf
-  <div class="row">
-  <div class="col-md-4"></div>
-  <div class="form-group col-md-4">
-  <label for="nis">NIS:</label>
-  <input type="text" class="form-control" name="nis">
-  </div>
-  </div>
-  <div class="row">
-  <div class="col-md-4"></div>
-  <div class="form-group col-md-4">
-  <label for="nama">Nama:</label>
-  <input type="text" class="form-control" name="nama">
-  </div>
-  </div>
-  <div class="row">
-  <div class="col-md-4"></div>
-  <div class="form-group col-md-4">
-  <label for="alamat">Alamat:</label>
-  <input type="text" class="form-control" name="alamat">
-  </div>
-  </div>
 
-  <div class="row">
-  <div class="col-md-4"></div>
-  <div class="form-group col-md-4">
-  <lable>Jenis Kelamin</lable>
-  <select name="jenis_kelamin">
-  <option value="1">Laki-Laki</option>
-  <option value="2">Perempuan</option>
-  </select>
-  </div>
-  </div>
-  <div class="row">
-  <div class="col-md-4"></div>
-  <div class="form-group col-md-4" style="margin-top:60px">
-  <button type="submit" class="btn btn-success">Submit</button>
-  </div>
-  </div>
-  </form>
-  </div>
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
+
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                            <li><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main class="py-4">
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Add Data Siswa</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ url('siswa') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="nis" class="col-sm-4 col-form-label text-md-right">Nis</label>
+
+                            <div class="col-md-6">
+                                <input id="nis" type="text" class="form-control{{ $errors->has('nis') ? ' is-invalid' : '' }}" name="nis" required autofocus>
+
+                                @if ($errors->has('nis'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('nis') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="nama" class="col-md-4 col-form-label text-md-right">Nama</label>
+
+                            <div class="col-md-6">
+                                <input id="nama" type="text" class="form-control{{ $errors->has('nama') ? ' is-invalid' : '' }}" name="nama" required>
+
+                                @if ($errors->has('nama'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('nama') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="alamat" class="col-md-4 col-form-label text-md-right">Alamat</label>
+
+                            <div class="col-md-6">
+                                <input id="alamat" type="text" class="form-control{{ $errors->has('alamat') ? ' is-invalid' : '' }}" name="alamat" required>
+
+                                @if ($errors->has('alamat'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('alamat') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="Jenis Kelamin" class="col-md-4 col-form-label text-md-right">Jenis Kelamin</label>
+
+                            <div class="col-md-6">
+                                <select name="jenis_kelamin"  class="form-control{{ $errors->has('jenis_kelamin') ? ' is-invalid' : '' }}">
+                                    <option value="1">Laki-Laki</option>
+                                    <option value="2">Perempuan</option>
+                                </select>
+
+                                @if ($errors->has('jenis_kelamin'))
+                                    <span class="invalid-feedback">
+                                        <strong>{{ $errors->first('jenis_kelamin') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+            
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Simpan
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+        </main>
+    </div>
+
+
 </body>
 </html>
