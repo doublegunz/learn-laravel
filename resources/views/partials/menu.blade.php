9564865aaadf013991b5a7840d8d06929d29cb6b
Gun Gun Priatna <gungunpriatna02@gmail.com>
2021-10-11 14:33:32 +0700

    feat: template ui

---
+++ resources/views/partials/menu.blade.php
@@ -0,0 +1,22 @@
+<div class="sidebar">
+    <nav class="sidebar-nav">
+
+        <ul class="nav">
+            <li class="nav-item">
+                <a href="{{ route('home') }}" class="nav-link ">
+                    <i class="nav-icon fas fa-fw fa-tachometer-alt"></i>
+                    Dashboard
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link"
+                    onclick="event.preventDefault(); document.getElementById('logoutform').submit();">
+                    <i class="nav-icon fas fa-fw fa-sign-out-alt"></i>
+                    Logout
+                </a>
+            </li>
+        </ul>
+
+    </nav>
+    <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+</div>
