fe0d32b097bda33cccc80cd5f989892f58b847e2
Gun Gun Priatna <gungunpriatna02@gmail.com>
2021-12-23 20:22:35 +0700

    test: create new user

--- tests/Feature/ManageUsersTest.php
+++ tests/Feature/ManageUsersTest.php
@@ -2,16 +2,60 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class ManageUsersTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     public function admin_can_create_new_user()
     {
-        $this->assertTrue(true);
+        // admin dalam status sudah login
+        $user = User::create([
+            'name' => 'admin',
+            'email' => 'admin@learn-laravel.com',
+            'password' => bcrypt('inigarahasia')
+        ]);
+
+        $this->actingAs($user);
+
+        // admin buka halaman daftar user
+        $this->visit('/user');
+
+        // admin klik tombol `Create User`
+        $this->click('Create User');
+
+
+        // tampil form `Create New User`
+        $this->seePageIs('/user/create');
+
+        $this->seeElement('form', [
+            'action' => url('/user')
+        ]);
+
+        // admin submit form berisi data user baru
+        $this->submitForm('Save', [
+            'name' => 'Gun Gun Priatna',
+            'email' => 'gungunpriatna@test.com',
+            'password' => 'inibukanrahasia12345'
+        ]);
+
+        // record ada di database
+        $this->seeInDatabase('users', [
+            'name' => 'Gun Gun Priatna',
+            'email' => 'gungunpriatna@test.com',
+        ]);
+
+        // lihat halaman ter-redirect
+        $this->seePageIs('/user');
+
+        $this->see('Gun Gun Priatna');
+        $this->see('gungunpriatna@test.com');
+
     }
 
     /** @test */
