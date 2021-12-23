a7e6e61c592a8dd13d003f28684a8215fd40cf5c
Gun Gun Priatna <gungunpriatna02@gmail.com>
2021-12-23 19:19:36 +0700

    init test list

---
+++ tests/Feature/ManageUsersTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ManageUsersTest extends TestCase
+{
+    /** @test */
+    public function admin_can_create_new_user()
+    {
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function admin_can_browser_users_index_page()
+    {
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function admin_can_edit_an_existing_user()
+    {
+        $this->assertTrue(true);
+    }
+
+    /** @test */
+    public function admin_can_delete_an_existing_user()
+    {
+        $this->assertTrue(true);
+    }
+}
